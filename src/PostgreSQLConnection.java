edc1562c945a7ffe9d63179beed55d24fa9ed404
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2024-09-09 10:05:20 +0100

    Connexion a la base de données

---
+++ src/PostgreSQLConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSQLConnection {
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/WorkPal";
+        String user = "postgres";
+        String password = "123";
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connexion réussie à la base de données!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
