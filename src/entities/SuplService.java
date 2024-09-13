cd5a92e59e8bd15f013c5878c17ac416052f26de
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2024-09-13 11:43:50 +0100

    WAR-6 supservice

--- src/entities/SuplService.java
+++ src/entities/SuplService.java
@@ -6,11 +6,13 @@ public class SuplService {
     private String type;
     private String details;
     private int prix;
+    private int gestionnaire_id;
 
-    public SuplService(String type, String details, int prix) {
+    public SuplService(String type, String details, int prix , int gestionnaire_id) {
         this.type = type;
         this.details = details;
         this.prix = prix;
+        this.gestionnaire_id = gestionnaire_id;
     }
 
 
@@ -44,6 +46,13 @@ public class SuplService {
     public void setPrix(int prix) {
         this.prix = prix;
     }
+    public int getGestionnaire_id() {
+        return gestionnaire_id;
+    }
+
+    public void setGestionnaire_id(int gestionnaire_id) {
+        this.gestionnaire_id = gestionnaire_id;
+    }
     @Override
     public String toString() {
         return "SuplService{" +
@@ -52,6 +61,7 @@ public class SuplService {
                 ", details='" + details + '\'' +
                 '}';
     }
+
 
 
 }
